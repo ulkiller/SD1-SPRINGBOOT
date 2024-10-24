980534b30839c0399eae6ccfe18e52c34a2362f5
ulkar <ulkersirinov@gmail.com>
2024-10-25 03:17:06 +0400

    salam

---
+++ src/main/kotlin/com/midterm/oop2/HelloController.kt
@@ -0,0 +1,13 @@
+package com.midterm.oop2
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    fun hello(): String {
+        return "Hello, World!"
+    }
+}
---
+++ src/main/kotlin/com/midterm/oop2/Oop2Application.kt
@@ -0,0 +1,11 @@
+package com.midterm.oop2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Oop2Application
+
+fun main(args: Array<String>) {
+    runApplication<Oop2Application>(*args)
+}
